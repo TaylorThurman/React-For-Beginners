9ba9fd6cc5a7074175b84a4238d7ce89e6a581a3
okH3reWeGo <taylor.john.thurman@outlook.com>
2019-11-15 22:06:03 -0600

    Finished Video 19

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -13,6 +13,11 @@ class App extends React.Component {
     };
     componentDidMount() {
         const { params } = this.props.match;
+        // first reinstate our localStorage
+        const localStorageRef = localStorage.getItem(params.storeId);
+        if (localStorageRef) {
+            this.setState({ order: JSON.parse(localStorageRef) })
+        }
         this.ref = base.syncState(`${params.storeId}/fishes`, {
             context: this,
             state: 'fishes'
@@ -21,6 +26,10 @@ class App extends React.Component {
 
     componentWillUnmount() {
         base.removeBinding(this.ref);
+    }
+
+    componentDidUpdate() {
+        localStorage.setItem(this.props.match.params.storeId, JSON.stringify(this.state.order));
     }
 
     addFish = (fish) => {
